5da76576001b4c3a0b88cc42705c423375bbf682
Jean Santos <jeansantos.sw@gmail.com>
2021-11-11 22:37:15 +0000

    Commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Header />
+  </div>
+</template>
+
+<script>
+// Styles global for site...
+import "./styles/global.css";
+
+//Import components for site...
+import Header from "./components/Header/Header";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>Hello world! I'm Header!</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+header {
+  background: var(--color-background-nav);
+  width: 100%;
+  height: 60px;
+}
+</style>
